262d39097727bea2b77042e105db6246149ef6a3
Nitch <hassan@pop-os.localdomain>
2023-07-15 16:43:47 +0500

    add & sub() added

--- rpn.cpp
+++ rpn.cpp
@@ -2,8 +2,8 @@
 #include "src/funcs.h"
 using namespace std;
 int main(){
-    cout <<"p for seeing result \n" 
-    << "h for entering a number\n";
+    cout <<"- p for seeing result \n" 
+    << "- h for entering a number\n";
     cout << "> ";
     setup();
 return 0;
